8ee44bf0104bacd6b514585cae6837b97275fadf
ionpulse <ionpulse@8548d682-b626-0410-82bf-d7f40f438c40>
2007-02-07 17:12:28 +0000

    Added class generation helper script.

---
+++ tools/classgen.py
@@ -0,0 +1,88 @@
+classTemplateTxt = """/**      
+ * A one line description of the class.
+ *
+ * #include "XX.h"
+ *
+ * A longer description.
+ *  
+ * @see something
+ *
+ * Project 404 2007
+ *
+ * Authors:
+ * Name, Date
+ */
+
+#ifndef XX_h
+#define XX_h
+
+// SYSTEM INCLUDES
+//
+
+// PROJECT INCLUDES
+//
+
+// LOCAL INCLUDES
+//
+
+// FORWARD REFERENCES
+//
+
+
+class XX
+{
+public:
+// LIFECYCLE
+
+   /**
+    * Default constructor.
+	*/
+   XX(void);
+
+   /**
+    * Copy constructor.
+	*
+	* @param from The value to copy to this object.
+	*/
+   XX(const XX& from);
+
+   /**
+    * Destructor.
+	*/
+   ~XX(void);
+
+// OPERATORS
+
+   /**
+    * Assignment operator.
+	*
+	* @param from The value to assign to this object.
+	*
+	* @return A reference to this object.
+	*/
+   XX&                     operator=(XX& from);  
+
+// OPERATIONS                       
+// ACCESS
+// INQUIRY
+
+protected:
+private:
+};
+
+// INLINE METHODS
+//
+
+// EXTERNAL REFERENCES
+//
+
+#endif  // _XX_h_"""
+
+className = raw_input( "Please enter the name of your class: " )
+
+classTemplateTxt = classTemplateTxt.replace( "XX", className )
+
+classfile = open( className + ".h", "w" )
+
+classfile.write( classTemplateTxt )
+classfile.close()
