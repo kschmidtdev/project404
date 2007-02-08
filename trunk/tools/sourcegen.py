93e1ff215dedb4add39279a02d07e2f152ffebcd
Mike.Malyuk <Mike.Malyuk@8548d682-b626-0410-82bf-d7f40f438c40>
2007-02-08 02:43:39 +0000

    Edited old py script to create script for source code. Also added readme which can be extended if/when we make new tools

---
+++ trunk/tools/sourcegen.py
@@ -0,0 +1,44 @@
+classTemplateTxt = """#include "XX.h"                                // class implemented
+
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+//============================= LIFECYCLE ====================================
+
+XX::XX()
+{
+}// XX
+
+XX::XX(const XX&)
+{
+}// XX
+
+XX::~XX()
+{
+}// ~XX
+
+
+//============================= OPERATORS ====================================
+
+XX& 
+XX::operator=(XX&);
+{
+   return *this;
+
+}// =
+
+//============================= OPERATIONS ===================================
+//============================= ACESS      ===================================
+//============================= INQUIRY    ===================================
+/////////////////////////////// PROTECTED  ///////////////////////////////////
+
+/////////////////////////////// PRIVATE    ///////////////////////////////////"""
+
+className = raw_input( "Please enter the name of your class: " )
+
+classTemplateTxt = classTemplateTxt.replace( "XX", className )
+
+classfile = open( className + ".cpp", "w" )
+
+classfile.write( classTemplateTxt )
+classfile.close()
