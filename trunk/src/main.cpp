7bf7ee0e4d7f25f763263d972f85e2d2592d7123
ionpulse <ionpulse@8548d682-b626-0410-82bf-d7f40f438c40>
2007-02-11 19:59:05 +0000

    Added GameRoot object, main.cpp now uses it as well, fixed a memory leak in SoundManager.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,79 +1,14 @@
-#include "Renderer/sdlrenderer.h"
-#include "ResourceManager/ResourceManager.h"
-#include "Renderer/ExampleRenderable.h"
-
-#include "InputManager.h"
-#include "ExampleEventListener.h"
-#include "Logger.h"
-#include "UIManager.h"
+#include <GameRoot.h>
 
 int main ( int argc, char** argv )
 {
-    // TODO: Clean all this up, make the darn GameRoot!
+    GameRoot ourGame;
 
-    Logger* logger = Logger::GetInstance( "tacLogFile.txt" );
+    ourGame.Initialize();
 
-    logger->Initialize();
+    ourGame.GameLoop();
 
-    ResourceManager* resManager = ResourceManager::GetInstance();
-    LogInfo( "ResourceManager started successfully" );
-
-    InputManager* inputManager = InputManager::GetInstance();
-
-    resManager->Initialize();
-
-    SDL_Surface* testSurface = 0;
-    testSurface = resManager->LoadTexture( "cb.bmp" );
-
-    ExampleRenderable dude;
-
-    SDLRenderer* renderer = SDLRenderer::GetInstance();
-
-    renderer->Initialize( 640, 480, 32 );
-    inputManager->Initialize();
-
-    renderer->AddToRenderQueue( &dude );
-
-    ExampleEventListener testListener;
-
-    inputManager->AddEventListener( &testListener );
-
-    // program main loop
-    bool done = false;
-    while (!done)
-    {
-        // message processing loop
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            // check for messages
-            switch (event.type)
-            {
-                // exit if the window is closed
-            case SDL_QUIT:
-                done = true;
-                break;
-
-                // check for keypresses
-            case SDL_KEYDOWN:
-                {
-                    inputManager->ProcessEvent( &event );
-                    // exit if ESCAPE is pressed
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = true;
-                    break;
-                }
-            } // end switch
-        } // end of message processing
-
-        //renderer.Update();
-        renderer->Draw();
-    }
-
-    inputManager->Shutdown();
-    resManager->Shutdown();
-    renderer->Shutdown();
-    logger->Shutdown();
+    ourGame.Shutdown();
 
     return 0;
 }
