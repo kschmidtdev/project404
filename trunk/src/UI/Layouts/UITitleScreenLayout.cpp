7befe962e191e8e1c64ebe6d2b54fd78f5826078
andrew.cc.osborne <andrew.cc.osborne@8548d682-b626-0410-82bf-d7f40f438c40>
2007-02-15 05:18:41 +0000

    Ascthetic upgrade for title page and main menu.  I'll put the new pic under 'newPic' in my www.sfu.ca/~aco1/newPic.zip directory

--- trunk/src/UI/Layouts/UITitleScreenLayout.cpp
+++ trunk/src/UI/Layouts/UITitleScreenLayout.cpp
@@ -7,10 +7,12 @@
  * Andrew Osborne, February 10 2007 | Initial Creation
  * Andrew Osborne, February 10 2007 | Documentation
  * Andrew Osborne, February 14 2007 | Added ProcessEvent method
+ * Andrew Osborne, February 14 2007 | Made final version for title screen
 */
 #include "UITitleScreenLayout.h"                                // class implemented
 #include "UIImage.h"
 #include "UIManager.h"
+#include "UIText.h"
 
 
 /////////////////////////////// PUBLIC ///////////////////////////////////////
@@ -21,8 +23,23 @@ UITitleScreenLayout::UITitleScreenLayout()
 {
     mName = "TitleScreen";
 
-    UIImage *temp = new UIImage("nicePic.bmp");
+    //UIImage *temp = new UIImage("nicePic.bmp");
+    UIImage *temp = new UIImage("castle.bmp");
+    temp->SetPos( Point(20, 16) );
     mElements.push_back(temp);
+
+    UIText *tempText = new UIText("SymTac", 100, 255, 0, 0);
+    tempText->SetPos( Point(120,50) );
+    //SDL_Surface *tempSur = tempText->GetElement();
+    //printf("title: width: %d, height: %d\n", tempSur->w, tempSur->h);
+    mElements.push_back(tempText);
+
+    tempText = new UIText("Press CONFIRM to advance", 24, 255, 0, 0);
+    tempText->SetPos( Point(165, 390) );
+    //tempSur = tempText->GetElement();
+    //printf("confirm: width: %d, height: %d\n", tempSur->w, tempSur->h);
+    mElements.push_back(tempText);
+
 
 }// UITitleScreenLayout
 
@@ -44,7 +61,6 @@ void UITitleScreenLayout::ProcessEvent( const InputManager::INPUTKEYS evt )
     switch (evt)
     {
         case InputManager::CONFIRM:
-            printf("pressed confirm\n");
             UIManager::GetInstance()->PushLayout("MainMenu");
             break;
         default:
