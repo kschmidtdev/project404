fb096e2ebc7e0731fa0d190e8fa6d0bd1ac96dd1
Mike.Malyuk <Mike.Malyuk@8548d682-b626-0410-82bf-d7f40f438c40>
2007-02-16 11:40:03 +0000

    AIControlTest works

---
+++ trunk/src/UnitTests/AIControlTest.h
@@ -0,0 +1,100 @@
+#ifndef __AICONTROLTEST_H
+#define __AICONTROLTEST_H
+
+#include <cxxtest/TestSuite.h>
+
+#include <../AI/AIControl.h>
+#include <../GameEngine/Level.h>
+#include <../Point.h>
+#include <../GameEngine/Character.h>
+//
+// A generated test suite: Just write tests!
+//
+
+class AIControlTest : public CxxTest::TestSuite
+{
+private:
+// any variables that could be shared between tests
+
+// Unit Tests:
+public:
+
+    // Called before all unit tests in this suite, remove if not needed
+    void setUp()
+    {
+
+    }
+
+    // Called after all unit tests in this suite, remove if note needed
+    void tearDown()
+    {
+
+    }
+
+    void testAI()
+    {
+        Level* level = new Level( 0 );
+        Point p(9,9);
+        AIControl ai(level, p);
+        level->SetState(Level::AIFREE);
+        //test point retrieval
+        Point spot = ai.DoAction();
+        vector<Character*> guys = level->GetEnemies();
+        vector<Character*>::iterator iter = guys.begin();
+        while(iter != guys.end())
+        {
+            if((*iter)->GetPoint() == spot)
+            {
+                break;
+            }
+            iter++;
+        }
+        //it exists in list
+        TS_ASSERT_EQUALS((*iter)->GetPoint(), spot);
+
+        level->OnAISelect(Point(1,9));
+        TS_ASSERT_EQUALS(level->ReturnState(), Level::AIMOVE);
+
+        Point another = ai.DoAction();
+        vector<Point> points = level->GetMoveArea();
+        vector<Point>::iterator ppiter;
+        ppiter = points.begin();
+        while(ppiter != points.end())
+        {
+            if((*ppiter) == another)
+            {
+                break;
+            }
+            ppiter++;
+        }
+        //it exists in list
+        TS_ASSERT_EQUALS((*ppiter), another);
+        level->OnAISelect(Point(0,9));
+        level->GetCurCharacter()->Move(Point(0,1));
+        TS_ASSERT_EQUALS(level->ReturnState(), Level::AIMOVE);
+        level->GetMovement();
+        level->OnAISelect(Point(0,1));
+
+
+        TS_ASSERT_EQUALS(level->ReturnState(), Level::AIATTACK);
+        Point spotted = ai.DoAction();
+        vector<Character*> guyss = level->GetEveryone();
+        vector<Character*>::iterator iterer = guyss.begin();
+        while(iterer != guyss.end())
+        {
+            if((*iterer)->GetPoint() == spotted)
+            {
+                break;
+            }
+            iterer++;
+        }
+        //it exists in list
+        TS_ASSERT_EQUALS((*iterer)->GetPoint(), spotted);
+
+    }
+
+};
+
+
+#endif // __AICONTROLTEST_H
+
