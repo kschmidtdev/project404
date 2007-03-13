e92f70d9a6276b23f9c19a0acd7db38dbceea073
andrew.cc.osborne <andrew.cc.osborne@8548d682-b626-0410-82bf-d7f40f438c40>
2007-03-13 21:55:17 +0000

    Started my unit tests (eeeeiiiii).  Created UIUnitTestHelp.cpp and only added it to the Unit Test build.  Also created unit test for UIElement and UIImage, many more tests to soon follow very quickly....
    Also fixed some bugs in OverMap and OverMapTile.  currently by default you can advance to the second level.  (if you didn't already get it, you'll now need the "victory.png" picture on my sfu-webspace)

---
+++ trunk/src/UnitTests/UIImageTest.h
@@ -0,0 +1,94 @@
+#ifndef __UIIMAGETEST_H
+#define __UIIMAGETEST_H
+
+#include <cxxtest/TestSuite.h>
+
+#include <UIImage.h>
+#include <UIUnitTestHelp.h>
+
+//
+// A generated test suite: Just write tests!
+//
+
+class UIImageTest : public CxxTest::TestSuite
+{
+private:
+// any variables that could be shared between tests
+    UIImage *testImage1;
+    UIImage *testImage2; // 2 different constructors
+    string testFileName;
+    UIUnitTestHelp* mUIUnitTester;
+
+// Unit Tests:
+public:
+
+    // Constructor
+    UIImageTest()
+    : testImage1( NULL ), testImage2( NULL ), mUIUnitTester( NULL )
+    {
+    }
+
+
+    // Called before all unit tests in this suite, remove if not needed
+    void setUp()
+    {
+
+        // Header for all UI Testing Elements
+        Logger::GetInstance( "unitTestLog.txt" );
+        Logger::GetInstance()->Initialize();
+        SDLRenderer::GetInstance()->Initialize( 480, 320, 32 );
+        ResourceManager::GetInstance()->Initialize();
+
+
+    }
+
+    // Called after all unit tests in this suite, remove if note needed
+    void tearDown()
+    {
+
+        ResourceManager::GetInstance()->Shutdown();
+        SDLRenderer::GetInstance()->Shutdown();
+        Logger::GetInstance()->Shutdown();
+
+    }
+
+    void testRenderSelf()
+    {
+
+        // Initializing Elements for test
+        testFileName = "charTile.png";
+        testImage1 = new UIImage();
+        testImage2 = new UIImage(testFileName);
+        mUIUnitTester = new UIUnitTestHelp( testImage1 , "UIImage" );
+
+        if ( (testImage1) && (testImage2) )
+        {
+
+            TS_ASSERT( testImage1->GetElement() == NULL );
+
+            TS_ASSERT( testImage2->GetElement() != NULL );
+
+            mUIUnitTester->RunBasicTest();
+            mUIUnitTester->SetNewElement( testImage2 );
+            mUIUnitTester->RunBasicTest();
+
+
+
+        }
+        else
+        {
+            TS_TRACE( "Images not initializing for some reason" );
+        }
+
+        delete testImage1;
+        delete testImage2;
+        delete mUIUnitTester;
+
+    }
+
+
+};
+
+
+#endif // __UIIMAGETEST_H
+
