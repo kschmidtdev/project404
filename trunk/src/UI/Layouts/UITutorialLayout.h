391cec5785b0c1ead3c0e68e74a8749826f8deda
andrew.cc.osborne <andrew.cc.osborne@8548d682-b626-0410-82bf-d7f40f438c40>
2007-03-24 07:18:52 +0000

    Made two updates.
    Made Tutorial Layout Functional:
    
    the pics are:
    www.sfu.ca/~aco1/images/tutorial_1.png
    www.sfu.ca/~aco1/images/tutorial_2.png
    www.sfu.ca/~aco1/images/tutorial_3.png
    (and they are lowercase 'png' this time)
    
    The other alteration is that I added a SetImage method to UIImage, that allows you to set a different filename to draw the image from.

--- trunk/src/UI/Layouts/UITutorialLayout.h
+++ trunk/src/UI/Layouts/UITutorialLayout.h
@@ -10,6 +10,7 @@
  * Authors:
  * Andrew Osborne, March 18 2007, Initial Creation
  * Karl Schmidt, March 22 2007      | Correcting include orders and paths
+ * Andrew Osborne, March 23 2007, Proper Implementation of functionality
  */
 
 #ifndef UITutorialLayout_h
@@ -27,7 +28,7 @@
 
 // FORWARD REFERENCES
 //
-
+class UIImage;
 
 class UITutorialLayout : public UILayout
 {
@@ -75,6 +76,20 @@ public:
 
 protected:
 // PROTECTED VARIABLES
+
+    // Tutorial page indexing
+    int mMinLayoutNum;
+    int mMaxLayoutNum;
+    int mCurLayoutNum;
+
+    // Image filename
+    std::string mLayoutFileNamePrefix;
+    std::string mLayoutFileNameSuffix;
+
+    // Tutorial Image
+    UIImage* mBack;
+
+
 private:
 // PRIVATE VARIABLES
 };
--- trunk/src/UI/UIImage.cpp
+++ trunk/src/UI/UIImage.cpp
@@ -6,6 +6,7 @@
  * Authors:
  * Andrew Osborne, February 11 2007 | Initial Creation
  * Karl Schmidt, March 22 2007      | Correcting include orders and paths
+ * Andrew Osborne, March 23 2007 | Added "SetImage" method to dynamically adjust image
  */
 
 #include "UIImage.h"                                // class implemented
@@ -39,6 +40,11 @@ UIImage::~UIImage()
 
 //============================= OPERATIONS ===================================
 //============================= ACCESS     ===================================
+
+void UIImage::SetImage(const std::string & filename)
+{
+    mElementImage = ResourceManager::GetInstance()->LoadTexture(filename);
+}
 //============================= INQUIRY    ===================================
 /////////////////////////////// PROTECTED  ///////////////////////////////////
 
