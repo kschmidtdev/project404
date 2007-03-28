b78242e6fe38dfbf8dd5cce65726b0f887fb32a7
Mike.Malyuk <Mike.Malyuk@8548d682-b626-0410-82bf-d7f40f438c40>
2007-03-28 03:42:37 +0000

    Modified items to hold a cost, modified city to hold items and be able to return them. UI should be able to hook into this.

--- src/GameEngine/City.cpp
+++ src/GameEngine/City.cpp
@@ -6,6 +6,7 @@
  * Authors:
  * Andrew Osborne, March 13 2007 | Initial Creation
  * Karl Schmidt, March 14 2007   | Added city ID auto-increment, removed default constructor (unused)
+ * Mike Malyuk,  March 27 2007   | Adding items to all cities. Fixing destructor to work with this.
  */
 #include "City.h"                                // class implemented
 
@@ -22,13 +23,28 @@ City::City(Point p, string fileName)
 {
     mMapPos = p;
     mImageFilename = fileName;
-
+    mArmoryItems.push_back(new ArmorItem("Chain Mail", 5));
+    mArmoryItems.push_back(new ArmorItem("Leather Jerkin", 2));
+    mArmoryItems.push_back(new ArmorItem("Steel Plate", 7));
+    mMarketItems.push_back(new WeaponItem("Blades of Steel", 3));
+    mMarketItems.push_back(new WeaponItem("Vorpal Sword", 5));
+    mMarketItems.push_back(new WeaponItem("Rat Flail", 1));
     mID = _idCount;
     ++_idCount;
 }
 
 City::~City()
 {
+    for(vector<Item*>::iterator item = mArmoryItems.begin(); item != mArmoryItems.end(); item++)
+    {
+        delete (*item);
+    }
+    for(vector<Item*>::iterator item = mMarketItems.begin(); item != mMarketItems.end(); item++)
+    {
+        delete (*item);
+    }
+    mArmoryItems.clear();
+    mMarketItems.clear();
 }// ~City
 
 
